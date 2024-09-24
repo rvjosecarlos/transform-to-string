d947801cf39c6899c41b7c1a2cea61bef4f8e38b
Carlos <rvjosecarlos@gmail.com>
2024-09-23 20:51:01 -0600

    test-5

--- src/controller/TransformToString.ts
+++ src/controller/TransformToString.ts
@@ -1,37 +1,23 @@
 import { Request, Response } from "express"
 import puppeteer from "puppeteer-core";
+import { chromium } from "playwright";
 
 export default async function TransformToString(req: Request , res: Response) {
     try {
         const { htmlString } = req.body;
 
-        const navegador = await puppeteer.launch({
-            executablePath: '/usr/bin/chromium-browser', // Ruta donde Render instala Chromium
-            headless: true, // Asegúrate de ejecutar en modo headless
-            args: ['--no-sandbox', '--disable-setuid-sandbox'], // Argumentos necesarios para Render
-        });
-        const paginaWeb = await navegador.newPage();
-        await paginaWeb.setContent(htmlString, { waitUntil: 'domcontentloaded' });
-        const pdf = await paginaWeb.pdf({
-            format: 'A4',
-            printBackground: true,
-            margin: {
-                top: '20mm',
-                bottom: '20mm',
-                left: '10mm',
-                right: '10mm' 
-            }
-        });
-        await navegador.close();
+        const browser = await chromium.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setContent(htmlString);
+        
+        const pdf = await page.pdf({ path: 'output.pdf', format: 'A4' });
+        await browser.close();
 
         const pdfBuffer = Buffer.from(pdf);
-
         const pdfBase64 = pdfBuffer.toString("base64");
-
         return res.json({success: true, data: pdfBase64});
     }
     catch(error) {
-        console.log(error);
         return res.json({success: false, error: 'Error al convertir'});
     }
 }
